f930e10d145630deba1c00300e6c6c2fe8437a79
QinzhiChen <qinzhic@gmail.com>
2022-08-23 10:40:34 -0500

    files modified

--- exp_mod.py
+++ exp_mod.py
@@ -15,7 +15,7 @@ def modeling(telco_x_train, telco_y_train, telco_x_validate, telco_y_validate):
     logrreg_m= LogisticRegression(random_state=123)
     dectre_m = DecisionTreeClassifier(max_depth=3, random_state=123)
     ranfor_m = RandomForestClassifier(max_depth=5, random_state=123)
-    knn_m= KNeighborsClassifier(n_neighbors=13, weights='uniform')
+    knn_m= KNeighborsClassifier(n_neighbors=15, weights='uniform')
     models = [logrreg_m, dectre_m, ranfor_m, knn_m]
     for model in models:
         model.fit(telco_x_train, telco_y_train)
